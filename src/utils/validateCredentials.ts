405721fe741229c5294cc30e6eac5779d7f1de6f
B-e-sa <joaospereira963@gmail.com>
2023-03-14 12:31:12 -0300

    add comment to cpf regex

--- src/utils/validateCredentials.ts
+++ src/utils/validateCredentials.ts
@@ -21,13 +21,14 @@ const validateCredentials = ({ cpf, phone, name, email }: ICredentials) => {
     if (!validate(email))
         errors.email = "invalid email";
 
+    // the regex will validate if the cpf doesn't contain spaces and is just numbers
     if (cpf?.length !== 11 || !cpf.match(/^\d+$/))
         errors.cpf = "invalid cpf";
 
     if (phone?.length !== 11)
         errors.phone = "invalid phone number";
 
-    // the regex will validate if the name isn't just spaces
+    // the regex will validate if the name doesn't just spaces
     if (name?.replace(/\s/g, '').length == 0)
         errors.name = "invalid name";
 
